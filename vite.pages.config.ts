ec03a0f9c5d0965f708fe147133e31838e800cfa
Justin Von Braun <info@thebalddude.co>
2026-08-03 12:55:44 -0400

    Connect shadowframe.tech

--- vite.pages.config.ts
+++ vite.pages.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   root: "github-pages",
-  base: "/afterdark-ai/",
+  base: "/",
   publicDir: "../public",
   plugins: [react()],
   build: {
